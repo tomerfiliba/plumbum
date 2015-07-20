bedc023b814cdb1eb72a8ab9aaba25a98ac967eb
Henry Schreiner (Muon Linux) <henryiii@physics.utexas.edu>
2015-07-20 09:39:54 -0500

    Making examples executable directly.
    Conflicts:
    	examples/color.py

--- examples/alignment.py
+++ examples/alignment.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python
 from plumbum import cli
 
 class App(cli.Application):
     #VERSION = "1.2.3"
     #x = cli.SwitchAttr("--lala")
     y = cli.Flag("-f")
-    
+
     def main(self, x, y):
         pass
-    
+
 @App.subcommand("bar")
 class Bar(cli.Application):
     z = cli.Flag("-z")
-    
+
     def main(self, z, w):
         pass
 
