d88bcef2e1a15d2ec8da85e9cb1fc1574c97a1c4
Henry's desktop Ubuntu <henryschreineriii@utexas.edu>
2015-07-29 13:39:07 -0500

    Dropped unneeded complexity for the colors module; it can just be the original factories.

--- plumbum/colors.py
+++ plumbum/colors.py
@@ -6,21 +6,12 @@ all the standard syntax for colors.
 
 import sys
 import os
-from types import ModuleType
 
-from plumbum.colorlib import ANSIStyle, StyleFactory
+from plumbum.colorlib import ansicolors
 
-class ColorModuleType(ModuleType, StyleFactory):
-    
-    def __init__(self, name, style):
-        ModuleType.__init__(self, name)
-        StyleFactory.__init__(self, style)
-        self.__path__ = os.path.abspath(os.path.dirname(__file__))
-
-mymodule = ColorModuleType(__name__, ANSIStyle)
 
 # Oddly, the order here matters for Python2, but not Python3
-sys.modules[__name__ + '.fg'] = mymodule.fg
-sys.modules[__name__ + '.bg'] = mymodule.bg
-sys.modules[__name__] = mymodule
+sys.modules[__name__ + '.fg'] = ansicolors.fg
+sys.modules[__name__ + '.bg'] = ansicolors.bg
+sys.modules[__name__] = ansicolors
 
