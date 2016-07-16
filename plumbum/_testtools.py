5d8dd2da264bc0a0be31812a793481d28f2b0080
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2016-07-16 11:17:45 +0200

    Moving from skip to xfail on Windows (for future improvements to compat)

--- plumbum/_testtools.py
+++ plumbum/_testtools.py
@@ -9,7 +9,7 @@ skip_without_chown = pytest.mark.skipif(not hasattr(os, "chown"),
 skip_without_tty = pytest.mark.skipif(not sys.stdin.isatty(),
         reason="Not a TTY")
 
-skip_on_windows = pytest.mark.skipif(sys.platform == "win32",
+skip_on_windows = pytest.mark.xfail(sys.platform == "win32",
         reason="Windows not supported for this test (yet)")
 
 xfail_on_pypy = pytest.mark.xfail(platform.python_implementation() == 'PyPy',
