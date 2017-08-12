df5b5fd00ce7755cf429a9f8c758e5c2bccfaab4
Ofer Koren <koreno@gmail.com>
2017-08-12 13:13:36 +0300

    cli: improved 'Set' validator supports csv and non-string types

--- plumbum/cli/__init__.py
+++ plumbum/cli/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
 from .switches import SwitchError, switch, autoswitch, SwitchAttr, Flag, CountOf, positional
-from .switches import Range, Set, ExistingDirectory, ExistingFile, NonexistentPath, Predicate
+from .switches import Range, Set, ExistingDirectory, ExistingFile, NonexistentPath, Predicate, CSV
 from .application import Application
 from .config import ConfigINI, Config
