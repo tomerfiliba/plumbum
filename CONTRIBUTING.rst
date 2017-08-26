65834d1ddece93157550585c254cdd91090f7980
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2017-08-26 00:20:46 -0700

    Update script for easier contributions

--- CONTRIBUTING.rst
+++ CONTRIBUTING.rst
@@ -9,7 +9,6 @@ Pull requests welcome! Please make sure you add tests (in an easy `pytest` forma
 
 Plumbum.cli prints various messages for the user. These can be localized into your local language; pull requests adding languages are welcome.
 
-To add a language, copy the file `plumbum/cli/i18n/messages.pot` to `plumbum/cli/i18n/locale/<lang>/LC_MESSAGES/<lang>.po`, and add your language. 
-
+To add a language, run `./translations.py` from the main github directory, and then copy the file `plumbum/cli/i18n/messages.pot` to `plumbum/cli/i18n/<lang>.po`, and add your language. Run `./translations.py` again to update the file you made (save first) and also create the needed files binary file.
 
 See `gettext: PMOTW3 <https://pymotw.com/3/gettext/>`_ for more info.
---
+++ translations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from plumbum import local, FG
+from plumbum.cmd import xgettext, msgmerge, msgfmt
+
+translation_dir = local.cwd / 'plumbum/cli/i18n'
+template = translation_dir / 'messages.pot'
+
+xgettext['--from-code',
+         'utf-8',
+         '-L', 'python',
+         '--keyword=T_',
+         '--package-name=Plumbum.cli',
+         '-o', template,
+         sorted([x - local.cwd for x in local.cwd / 'plumbum/cli' // '*.py']),
+         ] & FG
+
+for translation in translation_dir // '*.po':
+    lang = translation.stem
+    new_tfile = translation.with_suffix('.po.new')
+
+    # Merge changes to new file
+    (msgmerge[translation, template] > new_tfile) & FG
+
+    new_tfile.move(translation)
+
+    # Render new file into runtime output
+    local_dir = translation_dir / lang / 'LC_MESSAGES'
+    if not local_dir.exists():
+        local_dir.mkdir()
+    msgfmt['-o',  local_dir / 'plumbum.cli.mo', translation] & FG
+
+
