6250a33bdc6ae247a3892de5964757db9bf3af20
Henry Schreiner <HenrySchreinerIII@gmail.com>
2019-05-15 01:23:58 +0200

    Fixing py 2.6 again

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -5,23 +5,15 @@ echo $TRAVIS_PYTHON_VERSION
 # Listed twice to workaround Python 2.6
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     python$PY3 -m pip install --upgrade pip
-    python$PY3 -m pip install --upgrade pytest setuptools wheel
     python$PY3 -m pip install -r dev-requirements.txt
     python$PY3 -m pip install coveralls
     python$PY3 -m pip install -e .
 else
     pip install --upgrade pip
-    pip install --upgrade pytest setuptools wheel
     pip install -r dev-requirements.txt
     pip install coveralls
     pip install -e .
 fi
-
-python$PY3 -m pip install --upgrade pip
-python$PY3 -m pip install --upgrade pytest setuptools wheel
-python$PY3 -m pip install -r dev-requirements.txt
-python$PY3 -m pip install coveralls
-python$PY3 -m pip install -e .
 
 echo NoHostAuthenticationForLocalhost yes >> ~/.ssh/config
 echo StrictHostKeyChecking no >> ~/.ssh/config
