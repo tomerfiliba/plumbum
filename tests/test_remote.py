7333b5a2f8c5e1421676aece01e06422bcca87f2
Georg Simbrunner <g.simbrunner@gmail.com>
2019-08-01 00:00:05 +0200

    Add test for env variables with special chars

--- tests/test_remote.py
+++ tests/test_remote.py
@@ -257,6 +257,18 @@ s.close()
                 p = rem.which("dummy-executable")
                 assert p == rem.cwd / "not-in-path" / "dummy-executable"
 
+    @pytest.mark.parametrize(
+        "env",
+        ["lala", "-Wl,-O2 -Wl,--sort-common", "{{}}", "''", "!@%_-+=:", "'",
+         "`", "$", "\\"])
+    def test_env_special_characters(self, env):
+        with self._connect() as rem:
+            with pytest.raises(ProcessExecutionError):
+                rem.python("-c", "import os;print(os.environ['FOOBAR72'])")
+            rem.env["FOOBAR72"] = env
+            out = rem.python("-c", "import os;print(os.environ['FOOBAR72'])")
+            assert out.strip() == env
+
     def test_read_write(self):
         with self._connect() as rem:
             with rem.tempdir() as dir:
