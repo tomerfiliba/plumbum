6788d15b017d7d6a3cf1834c9b098db2719762a9
KOLANICH <KOLANICH@users.noreply.github.com>
2020-04-02 05:49:32 +0000

    Moved the metadata into setup.cfg. Dropped 2.6 (#497)

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -2,18 +2,11 @@
 
 echo $TRAVIS_PYTHON_VERSION
 
-# Listed twice to workaround Python 2.6
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    python$PY3 -m pip install --upgrade pip
-    python$PY3 -m pip install -r dev-requirements.txt
-    python$PY3 -m pip install coveralls
-    python$PY3 -m pip install -e .
-else
-    pip install --upgrade pip
-    pip install -r dev-requirements.txt
-    pip install 'coveralls;python_version>="2.7"'
-    pip install -e .
-fi
+pip$PY3 install --upgrade pip
+pip$PY3 install --upgrade -r dev-requirements.txt
+pip$PY3 install coveralls
+python$PY3 ./setup.py bdist_wheel
+pip$PY3 install --upgrade ./dist/*.whl
 
 echo NoHostAuthenticationForLocalhost yes >> ~/.ssh/config
 echo StrictHostKeyChecking no >> ~/.ssh/config
