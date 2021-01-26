c1bab2a8369bc05492ed06aa0a9c8301259615e8
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2021-01-26 00:44:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import codecs
-import os
-
 from setuptools import setup
-
-# Fix for building on non-Windows systems
-try:
-    codecs.lookup("mbcs")
-except LookupError:
-    ascii = codecs.lookup("ascii")
-    func = lambda name, enc=ascii: {True: enc}.get(name == "mbcs")
-    codecs.register(func)
 
 setup()
