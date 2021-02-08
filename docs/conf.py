edc1276e44a7447c5590e3c0fadc131576f3f667
Henry Schreiner <henryschreineriii@gmail.com>
2021-02-08 15:32:58 -0500

    docs: fix issue building

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@ import os
 import sys
 import time
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
