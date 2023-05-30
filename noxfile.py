2283af6d36998e4f75132912cfc70d89e5938aac
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2023-05-30 06:25:10 -0400

    ci: add 3.12 beta testing (#650)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import nox
 
-ALL_PYTHONS = ["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"]
+ALL_PYTHONS = ["3.6", "3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
 
 nox.options.sessions = ["lint", "tests"]
 
