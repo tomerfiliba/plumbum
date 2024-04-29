cc5d3686b8bbeee825ad73949611b8823d502fc7
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2024-04-29 01:47:31 -0400

    chore: clean up codespell (#682)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ def pylint(session):
     Run pylint.
     """
 
-    session.install(".", "paramiko", "ipython", "pylint~=3.0.2")
+    session.install(".", "paramiko", "ipython", "pylint~=3.1.0")
     session.run("pylint", "plumbum", *session.posargs)
 
 
